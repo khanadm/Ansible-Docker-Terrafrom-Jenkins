992ee83890385f55c94aad82535e8e7eb07a49cd
root <root@localhost.localdomain>
2023-06-21 12:42:28 +0530

    added env variable in config.php

--- php-application/config.php
+++ php-application/config.php
@@ -1,16 +1,16 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'mysql');
-define('DB_USERNAME', 'ongraph');
-define('DB_PASSWORD', 'ongraph');
-define('DB_NAME', 'ongraph');
- 
-/* Attempt to connect to MySQL database */
+// Database credentials. Assuming you have set environment variables for these values
+define('DB_SERVER', getenv('DB_SERVER'));
+define('DB_USERNAME', getenv('DB_USERNAME'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_NAME', getenv('DB_NAME'));
+
+// Attempt to connect to MySQL database
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
+
